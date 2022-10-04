022a28f0fcc860f24aadbf0983a16f94f13d369f
Paco Fernandez <pacofdez55@gmail.com>
2022-10-04 10:12:02 +0000

    First commit

---
+++ school/models/models.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class student(models.Model):
+     _name = 'school.student'
+     _description = 'The.students'
+     name = fields.Char(string = "Nombre", required = True)
+     year = fields.Integer()
+     topic_id = fields.Many2many("school.topic")
+
+
+class topic(models.Model):
+     _name  = 'school.topic'
+     _description = 'The topics'
+     name = fields.Char(string = "Topic name", required = True)
+     students_id = fields.Many2many("school.student")
+
+class course(models.Model):
+     _name  = 'school.course'
+     _description = 'Courses'
+     name = fields.Char()
+     topics = fields.Many2many('school.topic')
+     students = fields.Many2many('school.student')
+     repeaters = fields.Many2many(comodel_name='school.student', # El model en el que es relaciona
+                            relation='course_students_repeaters_rel', # (opcional) el nom del la taula en mig
+                            column1='course_id', # (opcional) el nom en la taula en mig de la columna d'aquest model
+                            column2='student_id')  # (opcional) el nom de la columna de l'altre model.
