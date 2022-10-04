d221061e43516f5f90271444b5689971e1b45607
Paco Fernandez <pacofdez55@gmail.com>
2022-10-04 10:55:39 +0000

    Creadas clases villa y grimorio

--- white_clover/models/models.py
+++ white_clover/models/models.py
@@ -8,4 +8,26 @@ class player(models.Model):
     _name = 'white_clover.player'
     _description = 'White Clover'
 
+    image = fields.Image(max_width = 200, max_height = 200)
+    name = fields.Char(String = "Nombre", required = True)
+    password = fields.Char()
+
+    grimorios = fields.One2many('white_clover-grimorios', 'player')
+
+class villa(models.Model):
+    _name = 'white_clover.villa'
+    _description = 'Villas'
+
     name = fields.Char(required = True)
+    image = fields.Image(max_width = 200, max_height = 200)
+    size = fields.Float()
+
+    grimorios = fields.One2many('white_clover-grimorios', 'villa')
+
+
+class grimorio(models.Model):
+    _name = 'white_clover.grimorio'
+    _description = 'Grimorios'
+
+    villas = fields.Many2One('white_clover.villa')
+    player = fields.Many2One('white_clover.player')
