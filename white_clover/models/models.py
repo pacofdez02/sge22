4eef8800159c515ec45cd0453fed7c12d60f1c3a
Paco Fernandez <pacofdez55@gmail.com>
2022-10-17 10:29:54 +0000

    white clover actualizado

--- white_clover/models/models.py
+++ white_clover/models/models.py
@@ -6,31 +6,67 @@ from odoo import models, fields, api
 
 class player(models.Model):
     _name = 'white_clover.player'
-    _description = 'White Clover'
+    _description = 'Player'
 
     image = fields.Image(max_width = 200, max_height = 200)
     name = fields.Char(String = "Nombre", required = True)
     password = fields.Char()
 
-    grimorios = fields.One2many('white_clover.grimorio', 'player')
+    name_village = fields.Char(required = True)
+    image_village = fields.Image(max_width = 200, max_height = 200)
+    size_village = fields.Float()
+
+    buildings = fields.One2many('white_clover.building', 'player_village')
+
+    grimoires = fields.One2many('white_clover.grimoire', 'player')
 
 
 
-
-class villa(models.Model):
-    _name = 'white_clover.villa'
-    _description = 'Villas'
+class npc_village(models.Model):
+    _name = 'white_clover.npc_village'
+    _description = 'NPC Village'
 
     name = fields.Char(required = True)
     image = fields.Image(max_width = 200, max_height = 200)
     size = fields.Float()
 
-    grimorios = fields.One2many('white_clover.grimorio', 'villa')
+    grimoires = fields.One2many('white_clover.grimoire', 'npc_village')
+    buildings = fields.One2many('white_clover.building', 'npc_village')
 
 
-class grimorio(models.Model):
-    _name = 'white_clover.grimorio'
-    _description = 'Grimorios'
+class building(models.Model):
+    _name = 'white_clover.building'
+    _description = 'Building'
 
-    villa = fields.Many2one('white_clover.villa')
+    name = fields.Char(required = True)
+    image = fields.Image(max_width = 200, max_height = 200)
+    size = fields.Float()
+
+    player_village = fields.Many2one('white_clover.player')
+    npc_village = fields.Many2one('white_clover.npc_village')
+
+    #type = fields.Selection([('1','magic_institute'),('2','creation_institute'),('3','wizard_institute')])
+
+    type = fields.Many2one('white_clover.building_type')
+    magic_institute = fields.Float(related = 'type.magic_institute')
+    creation_institute = fields.Float(related = 'type.creation_institute')
+    wizard_institute = fields.Float(related = 'type.wizard_institute')
+
+
+class building_type(models.Model):
+    _name = 'white_clover.building_type'
+
+    magic_institute = fields.Float()
+    creation_institute = fields.Float()
+    wizard_institute = fields.Float()
+
+
+
+
+
+class grimoire(models.Model):
+    _name = 'white_clover.grimoire'
+    _description = 'Grimoire'
+
     player = fields.Many2one('white_clover.player')
+    npc_village = fields.Many2one('white_clover.npc_village')
